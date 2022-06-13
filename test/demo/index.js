e2bd4722568da2809e2e2ff14825d4fe0eef39c6
Ayushman Chhabra <ayushmxn@outlook.com>
2022-06-13 16:19:40 -0400

    fix(test): update imports

--- test/demo/index.js
+++ test/demo/index.js
@@ -4,40 +4,67 @@ var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __commonJS = (cb, mod) => function __require() {
-  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
-};
+var __commonJS = (cb, mod) =>
+  function __require() {
+    return (
+      mod ||
+        (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod),
+      mod.exports
+    );
+  };
 var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
+  if ((from && typeof from === "object") || typeof from === "function") {
     for (let key of __getOwnPropNames(from))
       if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+        __defProp(to, key, {
+          get: () => from[key],
+          enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable,
+        });
   }
   return to;
 };
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target, mod));
+var __toESM = (mod, isNodeMode, target) => (
+  (target = mod != null ? __create(__getProtoOf(mod)) : {}),
+  __copyProps(
+    isNodeMode || !mod || !mod.__esModule
+      ? __defProp(target, "default", { value: mod, enumerable: true })
+      : target,
+    mod,
+  )
+);
 
 // node_modules/.pnpm/uniqid@5.4.0/node_modules/uniqid/index.js
 var require_uniqid = __commonJS({
-  "node_modules/.pnpm/uniqid@5.4.0/node_modules/uniqid/index.js"(exports, module2) {
-    var pid = typeof process !== "undefined" && process.pid ? process.pid.toString(36) : "";
+  "node_modules/.pnpm/uniqid@5.4.0/node_modules/uniqid/index.js"(
+    exports,
+    module2,
+  ) {
+    var pid =
+      typeof process !== "undefined" && process.pid
+        ? process.pid.toString(36)
+        : "";
     var address = "";
-    if (typeof __webpack_require__ !== "function" && typeof require !== "undefined") {
-      mac = "", os = require("os");
-      if (os.networkInterfaces)
-        networkInterfaces = os.networkInterfaces();
+    if (
+      typeof __webpack_require__ !== "function" &&
+      typeof require !== "undefined"
+    ) {
+      (mac = ""), (os = require("os"));
+      if (os.networkInterfaces) networkInterfaces = os.networkInterfaces();
       if (networkInterfaces) {
-        loop:
-          for (let interface_key in networkInterfaces) {
-            const networkInterface = networkInterfaces[interface_key];
-            const length = networkInterface.length;
-            for (i = 0; i < length; i++) {
-              if (networkInterface[i] !== void 0 && networkInterface[i].mac && networkInterface[i].mac != "00:00:00:00:00:00") {
-                mac = networkInterface[i].mac;
-                break loop;
-              }
+        loop: for (let interface_key in networkInterfaces) {
+          const networkInterface = networkInterfaces[interface_key];
+          const length = networkInterface.length;
+          for (i = 0; i < length; i++) {
+            if (
+              networkInterface[i] !== void 0 &&
+              networkInterface[i].mac &&
+              networkInterface[i].mac != "00:00:00:00:00:00"
+            ) {
+              mac = networkInterface[i].mac;
+              break loop;
             }
           }
+        }
         address = mac ? parseInt(mac.replace(/\:|\D+/gi, "")).toString(36) : "";
       }
     }
@@ -45,55 +72,106 @@ var require_uniqid = __commonJS({
     var os;
     var networkInterfaces;
     var i;
-    module2.exports = module2.exports.default = function(prefix, suffix) {
-      return (prefix ? prefix : "") + address + pid + now().toString(36) + (suffix ? suffix : "");
+    module2.exports = module2.exports.default = function (prefix, suffix) {
+      return (
+        (prefix ? prefix : "") +
+        address +
+        pid +
+        now().toString(36) +
+        (suffix ? suffix : "")
+      );
     };
-    module2.exports.process = function(prefix, suffix) {
-      return (prefix ? prefix : "") + pid + now().toString(36) + (suffix ? suffix : "");
+    module2.exports.process = function (prefix, suffix) {
+      return (
+        (prefix ? prefix : "") +
+        pid +
+        now().toString(36) +
+        (suffix ? suffix : "")
+      );
     };
-    module2.exports.time = function(prefix, suffix) {
-      return (prefix ? prefix : "") + now().toString(36) + (suffix ? suffix : "");
+    module2.exports.time = function (prefix, suffix) {
+      return (
+        (prefix ? prefix : "") + now().toString(36) + (suffix ? suffix : "")
+      );
     };
     function now() {
       var time = Date.now();
       var last = now.last || time;
-      return now.last = time > last ? time : last + 1;
+      return (now.last = time > last ? time : last + 1);
     }
-  }
+  },
 });
 
 // dist/index.js
 var import_uniqid = __toESM(require_uniqid(), 1);
-var m = (t, e, ...r) => {
+var g = (t, e, ...n) => {
   if (typeof t != "string" && Array.isArray(t())) {
     let o = new DocumentFragment();
-    return o.append(...r), o;
+    return o.append(...n), o;
   } else {
-    if (typeof t != "string" && !Array.isArray(t()))
-      return t();
+    if (typeof t != "string" && !Array.isArray(t())) return t();
     if (typeof t == "string") {
       let o = document.createElement(t);
-      return e !== null && Object.entries(e).forEach(([a, s]) => {
-        if (a.startsWith("on") && a.toLowerCase() in window && o.addEventListener(a.toLowerCase().substring(2), s), a === "style") {
-          let n = "";
-          for (let i in s)
-            n += `${i.replace(/([A-Z])/g, (f) => `-${f[0].toLowerCase()}`)}: ${s[i]};
+      return (
+        e !== null &&
+          Object.entries(e).forEach(([s, a]) => {
+            if (
+              (s.startsWith("on") &&
+                s.toLowerCase() in window &&
+                o.addEventListener(s.toLowerCase().substring(2), a),
+              s === "style")
+            ) {
+              let r = "";
+              for (let i in a)
+                r += `${i.replace(
+                  /([A-Z])/g,
+                  (u) => `-${u[0].toLowerCase()}`,
+                )}: ${a[i]};
 `;
-          o.setAttribute(a, n);
-        } else
-          o.setAttribute(a, s.toString());
-      }), r.forEach((a) => u(o, a)), o;
-    } else
-      return document.createElement("div");
+              o.setAttribute(s, r);
+            } else o.setAttribute(s, a.toString());
+          }),
+        n.forEach((s) => l(o, s)),
+        o
+      );
+    } else return document.createElement("div");
   }
 };
-var u = (t, e) => {
-  Array.isArray(e) ? e.forEach((r) => u(t, r)) : typeof e == "string" ? t.appendChild(document.createTextNode(e)) : typeof e == "number" ? t.appendChild(document.createTextNode(e.toString())) : (e == null ? void 0 : e.nodeType) === 1 && t.appendChild(e);
+var l = (t, e) => {
+  Array.isArray(e)
+    ? e.forEach((n) => l(t, n))
+    : typeof e == "string"
+    ? t.appendChild(document.createTextNode(e))
+    : typeof e == "number"
+    ? t.appendChild(document.createTextNode(e.toString()))
+    : (e == null ? void 0 : e.nodeType) === 1 && t.appendChild(e);
 };
-var l = m;
-var c = (t, ...e) => e;
-var d = c;
-var D = { createElement: l, createFragment: d };
+var d = g;
+var p = (t, ...e) => e;
+var c = p;
+var L = { createElement: d, createFragment: c };
+
+// src/createEffect.ts
+var createEffect = (fn, deps) => {
+  window.onload = () => {
+    if (Array.isArray(deps) && deps.length === 0) {
+      fn();
+    } else {
+      for (const dep of deps) {
+        const target = document.getElementById(dep);
+        const config = { attributes: true, childList: true, subtree: true };
+        const callback = (mutations) => {
+          for (const _ of mutations) {
+            fn();
+          }
+        };
+        const observer = new MutationObserver(callback);
+        observer.observe(target, config);
+      }
+    }
+  };
+};
+var createEffect_default = createEffect;
 
 // src/createElement.ts
 var createElement = (tag, props, ...children) => {
@@ -114,7 +192,10 @@ var createElement = (tag, props, ...children) => {
         if (key === "style") {
           let style = "";
           for (const obj in value) {
-            style += `${obj.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`)}: ${value[obj]};
+            style += `${obj.replace(
+              /([A-Z])/g,
+              (g2) => `-${g2[0].toLowerCase()}`,
+            )}: ${value[obj]};
 `;
           }
           element.setAttribute(key, style);
@@ -158,7 +239,7 @@ var createState = (initialValue) => {
     if (document.getElementById(id) === null) {
       return {
         key: id,
-        value: initialValue
+        value: initialValue,
       };
     } else {
       let typedValue = document.getElementById(id)?.innerText;
@@ -182,7 +263,7 @@ var createState = (initialValue) => {
       }
       return {
         key: id,
-        value: typedValue
+        value: typedValue,
       };
     }
   };
@@ -201,21 +282,44 @@ var createState_default = createState;
 // src/index.ts
 var src_default = {
   createElement: createElement_default,
-  createFragment: createFragment_default
+  createFragment: createFragment_default,
 };
 
 // test/demo/examples/Counter.tsx
 var Counter = () => {
   let [count, setCount] = createState_default(0);
-  return /* @__PURE__ */ src_default.createElement(src_default.createFragment, null, /* @__PURE__ */ src_default.createElement("button", {
-    onClick: () => setCount(count().value - 1)
-  }, "-"), /* @__PURE__ */ src_default.createElement("span", {
-    id: count().key
-  }, count().value), /* @__PURE__ */ src_default.createElement("button", {
-    onClick: () => setCount(count().value + 1)
-  }, "+"));
+  createEffect_default(() => {
+    console.log("Count: ", count().value);
+  }, [count().key]);
+  return /* @__PURE__ */ src_default.createElement(
+    src_default.createFragment,
+    null,
+    /* @__PURE__ */ src_default.createElement(
+      "button",
+      {
+        onClick: () => setCount(count().value - 1),
+      },
+      "-",
+    ),
+    /* @__PURE__ */ src_default.createElement(
+      "span",
+      {
+        id: count().key,
+      },
+      count().value,
+    ),
+    /* @__PURE__ */ src_default.createElement(
+      "button",
+      {
+        onClick: () => setCount(count().value + 1),
+      },
+      "+",
+    ),
+  );
 };
 var Counter_default = Counter;
 
 // test/demo/index.tsx
-document.getElementById("root")?.appendChild(/* @__PURE__ */ D.createElement(Counter_default, null));
+document
+  .getElementById("root")
+  ?.appendChild(/* @__PURE__ */ L.createElement(Counter_default, null));
