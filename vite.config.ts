7abbf4b9d5311e4a44a09ca8949ee6aace8c62f3
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-06-27 22:12:38 +0530

    chore(ci): add vitest coverage action (#556)
    * Let Volta manage Node setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     test: {
+        coverage: {
+            provider: 'v8',
+            reporter: ['json', 'json-summary'],
+            reportOnFailure: true,
+        },
         globals: true,
         environment: 'jsdom',
         setupFiles: './test/setup.ts',
