f7fc2aa31c70f099b27e4d835f97b9d481ccf531
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-01-11 15:05:18 +0530

    chore(test): revert vite example

--- templates/vite/main.tsx
+++ templates/vite/main.tsx
@@ -1,26 +1,11 @@
-import VSX, { createRouter } from "../../src/index";
+import VSX from "vsx";
 
 function App(): HTMLElement {
-
-    const router = createRouter([
-        {
-            path: '/',
-            element: '<span>main</span>'
-        },
-        {
-            path: '/ping',
-            element: '<span>pong</span>'
-        }
-    ]);
 
     return (
         <div>
             <div>
                 Hello, World! Is anyone out there?
-                <br /><br />
-                <button onClick={() => {
-                    router('/');
-                }}>/ping</button>
             </div>
         </div>
     );
