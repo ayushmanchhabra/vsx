9f376ee21e20a6e84212aa30f1686435921731b5
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-01-11 15:02:03 +0530

    chore(test): add tests cases

--- templates/esbuild/main.tsx
+++ templates/esbuild/main.tsx
@@ -1,26 +1,55 @@
 import VSX, { createRouter } from "../../src/index";
 
-function App(): HTMLElement {
+const router = createRouter([
+    {
+        path: '/',
+        element: <App />
+    },
+    {
+        path: '/page1',
+        element: '<div>Welcome to Page 2</div>'
+    },
+    {
+        path: '/page2',
+        element: <Page2 />
+    },
+    {
+        path: '/page3',
+        element: '<div>Welcome to Page 3. Reloading leads to 404 Not Found.</div>'
+    }
+]);
 
-    const router = createRouter([
-        {
-            path: '/',
-            element: '<span>main</span>'
-        },
-        {
-            path: '/ping',
-            element: '<span>pong</span>'
-        }
-    ]);
+function Page2(): HTMLElement {
+    return (
+        <div>
+            <div>
+                Welcome to Page 2. 
+                <br /><br />
+                <button onClick={() => {
+                    router('/');
+                }}>Go to main page</button>
+                <button onClick={() => {
+                    router('/page2');
+                }}>Go to page 1</button>
+            </div>
+        </div>
+    );
+}
+
+function App(): HTMLElement {
 
     return (
         <div>
             <div>
-                Hello, World! Is anyone out there?
+                Welcome to App
                 <br /><br />
                 <button onClick={() => {
-                    router('/ping');
-                }}>/ping</button>
+                    router('/page1');
+                }}>Go to page 1</button>
+                <br /><br />
+                <button onClick={() => {
+                    router('/page2');
+                }}>Go to page 2</button>
             </div>
         </div>
     );
